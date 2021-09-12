f9d761bd27a75c70f882937e501e751ef89040df
Bryan Hernandez <proanthonyhdez@gmail.com>
2021-09-12 03:37:55 -0600

    Cambios en prod

--- ExpClinicoApi/DbContextSystem.cs
+++ ExpClinicoApi/DbContextSystem.cs
@@ -118,9 +118,9 @@ namespace ExpClinicoApi
             {
 
                 //optionsBuilder.UseMySQL("server=remotemysql.com;user=3ExE2H1Roh;port=3306;database=3ExE2H1Roh;password='Zr6eXf4jyi';");
-                //optionsBuilder.UseMySQL("server=sql3.freemysqlhosting.net;user=sql3435510;port=3306;database=sql3435510;password='XzxuMuNzFF';");
+                optionsBuilder.UseMySQL("server=sql3.freemysqlhosting.net;user=sql3435510;port=3306;database=sql3435510;password='XzxuMuNzFF';");
 
-                optionsBuilder.UseMySQL("server=localhost;user=root;database=clinica;port=3306;password='';");
+                //optionsBuilder.UseMySQL("server=localhost;user=root;database=clinica;port=3306;password='';");
                 //optionsBuilder.UseMySQL("server=db4free.net;user=zr6exf4jyi;port=3306;database=clinicazr6exf4jy;password='zr6exf4jyi';");
             }
         }
